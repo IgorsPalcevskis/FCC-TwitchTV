6d7df99634873af37ada27f1801241b40f7c7904
Igors Palcevskis <igors.palcevskis@gmail.com>
2017-10-31 14:37:34 +0000

    array/loop
    working on array and loop for ajax api call

--- javascript.js
+++ javascript.js
@@ -1,31 +1,36 @@
 $(document).ready(function(){
+   /* var channelsArray = ['cohhcarnage','the_lazy_peon','mfpallytime','neebsgaming','koyo','strippin','zethiann','forcegaming'];
+  for (var i = 0; i < channelsArray.length; i++) {
+    text += cars[i] + "<br>";
+  }
+  */
   $.ajax({
-    url:'https://api.twitch.tv/kraken/streams/the_lazy_peon/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
+    url:'https://api.twitch.tv/kraken/streams/cohhcarnage/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
     success: function(response) {
       if (response.stream) {
         var logo = response.stream.channel.logo;
         var channel_name = response.stream.channel.display_name;
         var channel_status = response.stream.channel.game;
         var channel_banner = response.stream.channel.profile_banner;
-        $('#channel1_logo').attr('src',logo);
-        $('#channel1_name').text(channel_name);
-        $('#channel1_status').text(channel_status);
-        $('#channel1_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
-        $('#channel1_on_off').text('Online').css('color','green');
+        $('#channel_logo').attr('src',logo);
+        $('#channel_name').text(channel_name);
+        $('#channel_status').text(channel_status);
+        $('#channel_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
+        $('#channel_on_off').text('Online').css('color','green');
       }else{
         $.ajax({
-          url: 'https://api.twitch.tv/kraken/channels/the_lazy_peon/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
+          url: 'https://api.twitch.tv/kraken/channels/cohhcarnage/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
           success: function (response) {
             console.log(response);
             var logo = response.logo;
             var channel_name = response.display_name;
             var channel_status = response.game;
             var channel_banner = response.profile_banner;
-            $('#channel1_logo').attr('src',logo);
-            $('#channel1_name').text(channel_name);
-            $('#channel1_status').text(channel_status);
-            $('#channel1_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
-            $('#channel1_on_off').text('Offline').css('color','red');
+            $('#channel_logo').attr('src',logo);
+            $('#channel_name').text(channel_name);
+            $('#channel_status').text(channel_status);
+            $('#channel_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
+            $('#channel_on_off').text('Offline').css('color','red');
           },
         });
       }
