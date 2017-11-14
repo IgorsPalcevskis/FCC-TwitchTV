89f95c86ff787ff80c7b9743f77d6d4b9707cd68
Igors Palcevskis <igors.palcevskis@gmail.com>
2017-11-14 14:45:51 +0000

    failed JS code
    commiting this to revert

--- javascript.js
+++ javascript.js
@@ -5,8 +5,9 @@ $(document).ready(function(){
   }
   */
   $.ajax({
-    url:'https://api.twitch.tv/kraken/streams/cohhcarnage/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
+    url:'https://api.twitch.tv/kraken/streams/mfpallytime/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
     success: function(response) {
+      console.log(response);
       if (response.stream) {
         var logo = response.stream.channel.logo;
         var channel_name = response.stream.channel.display_name;
@@ -17,22 +18,17 @@ $(document).ready(function(){
         $('#channel_status').text(channel_status);
         $('#channel_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
         $('#channel_on_off').text('Online').css('color','green');
-      }else{
-        $.ajax({
-          url: 'https://api.twitch.tv/kraken/channels/cohhcarnage/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
-          success: function (response) {
-            console.log(response);
-            var logo = response.logo;
-            var channel_name = response.display_name;
-            var channel_status = response.game;
-            var channel_banner = response.profile_banner;
-            $('#channel_logo').attr('src',logo);
-            $('#channel_name').text(channel_name);
-            $('#channel_status').text(channel_status);
-            $('#channel_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
+      }
+      else{
+            var logo_off = response.logo;
+            var channel_name_off = response.display_name;
+            var channel_status_off = response.game;
+            var channel_banner_off = response.profile_banner;
+            $('#channel_logo').attr('src',logo_off);
+            $('#channel_name').text(channel_name_off);
+            $('#channel_status').text(channel_status_off);
+            $('#channel_banner').css({'background-image':'url(' + channel_banner_off + ')', 'background-size':'cover'});
             $('#channel_on_off').text('Offline').css('color','red');
-          },
-        });
       }
     },
   });
