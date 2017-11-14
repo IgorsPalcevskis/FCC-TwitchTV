a0a12b9ea9459c31d1da2bd340287dc484e0f3fa
Igors Palcevskis <igors.palcevskis@gmail.com>
2017-11-14 14:46:31 +0000

    Revert "array/loop"
    This reverts commit 6d7df99634873af37ada27f1801241b40f7c7904.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-   /* var channelsArray = ['cohhcarnage','the_lazy_peon','mfpallytime','neebsgaming','koyo','strippin','zethiann','forcegaming'];
-  for (var i = 0; i < channelsArray.length; i++) {
-    text += cars[i] + "<br>";
-  }
-  */
   $.ajax({
+<<<<<<< HEAD
     url:'https://api.twitch.tv/kraken/streams/mfpallytime/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
+=======
+    url:'https://api.twitch.tv/kraken/streams/the_lazy_peon/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
+>>>>>>> parent of 6d7df99... array/loop
     success: function(response) {
       console.log(response);
       if (response.stream) {
@@ -13,6 +12,7 @@ $(document).ready(function(){
         var channel_name = response.stream.channel.display_name;
         var channel_status = response.stream.channel.game;
         var channel_banner = response.stream.channel.profile_banner;
+<<<<<<< HEAD
         $('#channel_logo').attr('src',logo);
         $('#channel_name').text(channel_name);
         $('#channel_status').text(channel_status);
@@ -29,6 +29,29 @@ $(document).ready(function(){
             $('#channel_status').text(channel_status_off);
             $('#channel_banner').css({'background-image':'url(' + channel_banner_off + ')', 'background-size':'cover'});
             $('#channel_on_off').text('Offline').css('color','red');
+=======
+        $('#channel1_logo').attr('src',logo);
+        $('#channel1_name').text(channel_name);
+        $('#channel1_status').text(channel_status);
+        $('#channel1_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
+        $('#channel1_on_off').text('Online').css('color','green');
+      }else{
+        $.ajax({
+          url: 'https://api.twitch.tv/kraken/channels/the_lazy_peon/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
+          success: function (response) {
+            console.log(response);
+            var logo = response.logo;
+            var channel_name = response.display_name;
+            var channel_status = response.game;
+            var channel_banner = response.profile_banner;
+            $('#channel1_logo').attr('src',logo);
+            $('#channel1_name').text(channel_name);
+            $('#channel1_status').text(channel_status);
+            $('#channel1_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
+            $('#channel1_on_off').text('Offline').css('color','red');
+          },
+        });
+>>>>>>> parent of 6d7df99... array/loop
       }
     },
   });
