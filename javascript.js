ae309a7b75f1c32765811aea7ddf453b0cd4c887
Igors Palcevskis <igors.palcevskis@gmail.com>
2017-11-16 14:58:41 +0000

    html/js
    removed HTML element placeholders;
    added javascript to generate html

--- javascript.js
+++ javascript.js
@@ -1,37 +1,35 @@
 $(document).ready(function(){
-/* var channelsArray = ['cohhcarnage','the_lazy_peon','mfpallytime','neebsgaming','koyo','strippin','zethiann','forcegaming'];
-for (var i = 0; i < channelsArray.length; i++) {
- text += cars[i] + "<br>";
-}
-*/
+var channelsArray = ['cohhcarnage','the_lazy_peon','mfpallytime','neebsgaming','koyo','strippin','zethiann','forcegaming'];
   $.ajax({
-    url:'https://api.twitch.tv/kraken/streams/mfpallytime/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
+    url:'https://api.twitch.tv/kraken/streams/'+ channelsArray[0] +'/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
     success: function(response) {
-      console.log(response);
       if (response.stream) {
         var logo = response.stream.channel.logo;
         var channel_name = response.stream.channel.display_name;
         var channel_status = response.stream.channel.game;
         var channel_banner = response.stream.channel.profile_banner;
+        var result = '';
+        result += '<div class="channel_frame col-lg-6 col-md-6 col-sm-12 col-xs-12"><div class="padding_div"><div class="channel_banner" id="channel_banner"></div><p id="channel_on_off">Offline</p><img id="channel_logo" class="logo_img" src="" alt="channel logo"><h2 id="channel_name"></h2><p id="channel_status"></p></div></div>';
+        $('.row').html(result);
         $('#channel_logo').attr('src',logo);
         $('#channel_name').text(channel_name);
         $('#channel_status').text(channel_status);
         $('#channel_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
         $('#channel_on_off').text('Online').css('color','green');
-      }
-      else{
+      } else{
         $.ajax({
-          url: 'https://api.twitch.tv/kraken/channels/mfpallytime/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
+          url: 'https://api.twitch.tv/kraken/channels/'+ channelsArray[0] +'/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
           success: function (response) {
             console.log(response);
             var logo = response.logo;
             var channel_name = response.display_name;
-            var channel_status = response.game;
             var channel_banner = response.profile_banner;
-            $('#channel1_logo').attr('src',logo);
-            $('#channel1_name').text(channel_name);
-            $('#channel1_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
-            $('#channel1_on_off').text('Offline').css('color','red');
+            result += '<div class="channel_frame col-lg-6 col-md-6 col-sm-12 col-xs-12"><div class="padding_div"><div class="channel_banner" id="channel_banner"></div><p id="channel_on_off">Offline</p><img id="channel_logo" class="logo_img" src="" alt="channel logo"><h2 id="channel_name"></h2><p id="channel_status"></p></div></div>';
+            $('.row').html(result);
+            $('#channel_logo').attr('src',logo);
+            $('#channel_name').text(channel_name);
+            $('#channel_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
+            $('#channel_on_off').text('Offline').css('color','red');
           },
         });
       }
