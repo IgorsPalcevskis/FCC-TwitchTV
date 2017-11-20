e56569e36ea258c1cc71de2ac7dfb5f6ad387c9e
Igors Palcevskis <igors.palcevskis@gmail.com>
2017-11-20 14:55:16 +0000

    added function
    *put ajax call within function;
    *added default twitch background to project

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 var channelsArray = ['cohhcarnage','the_lazy_peon','mfpallytime','neebsgaming','koyo','zethiann','forcegaming'];
 
-for (var i = 0; i < channelsArray.length; i++) {  /* loop begins */
-
-(function (i) {
+function sendRequest(i) {
   console.log(channelsArray[i]);
   $.ajax({
     url:'https://api.twitch.tv/kraken/streams/'+ channelsArray[i] +'/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
@@ -15,7 +13,7 @@ for (var i = 0; i < channelsArray.length; i++) {  /* loop begins */
         var channel_banner = response.stream.channel.profile_banner;
         var result = '';
         result += '<div class="channel_frame col-lg-6 col-md-6 col-sm-12 col-xs-12"><div class="padding_div"><div class="channel_banner" id="channel_banner"></div><p id="channel_on_off">Offline</p><img id="channel_logo" class="logo_img" src="" alt="channel logo"><h2 id="channel_name"></h2><p id="channel_status"></p></div></div>';
-        $('.row').html(result);
+        //$('.row').html(result);
         $('#channel_logo').attr('src',logo);
         $('#channel_name').text(channel_name);
         $('#channel_status').text(channel_status);
@@ -26,14 +24,19 @@ for (var i = 0; i < channelsArray.length; i++) {  /* loop begins */
           url: 'https://api.twitch.tv/kraken/channels/'+ channelsArray[i] +'/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
           success: function (response) {
             console.log(response);
-            var logo = response.logo;
-            var channel_name = response.display_name;
-            var channel_banner = response.profile_banner;
+            logo = response.logo;
+            channel_name = response.display_name;
+            channel_banner = response.profile_banner;
+            result = '';
             result += '<div class="channel_frame col-lg-6 col-md-6 col-sm-12 col-xs-12"><div class="padding_div"><div class="channel_banner" id="channel_banner"></div><p id="channel_on_off">Offline</p><img id="channel_logo" class="logo_img" src="" alt="channel logo"><h2 id="channel_name"></h2><p id="channel_status"></p></div></div>';
-            $('.row').html(result);
+            //$('.row').html(result);
             $('#channel_logo').attr('src',logo);
             $('#channel_name').text(channel_name);
-            $('#channel_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
+            if (channel_banner) {
+              $('#channel_banner').css({'background-image':'url(' + channel_banner + ')', 'background-size':'cover'});
+            }else {
+              $('#channel_banner').css({'background-image':'url(imgFolder/banner.jpg)', 'background-size':'cover'});
+            }
             $('#channel_on_off').text('Offline').css('color','red');
           },
         });
@@ -41,7 +44,11 @@ for (var i = 0; i < channelsArray.length; i++) {  /* loop begins */
     },
   });
 
-})(i);
-} /* loop ends */
+} // functions sendRequest() end
+
+
+for (var i = 0; i < channelsArray.length; i++) {
+  sendRequest(i);
+}
 
 });
