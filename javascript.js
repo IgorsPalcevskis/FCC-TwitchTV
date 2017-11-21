614e8e50f41eedf5512bdb6df7833297a1d5a0c8
Igors Palcevskis <igors.palcevskis@gmail.com>
2017-11-21 16:54:12 +0000

    ajax call for loop
    attempting to succesfully for loop through ajax calls and write new set of html elements for each call

--- javascript.js
+++ javascript.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
-var channelsArray = ['cohhcarnage','the_lazy_peon','mfpallytime','neebsgaming','koyo','zethiann','forcegaming'];
-
+var channelsArray = ['cohhcarnage','the_lazy_peon','mfpallytime','neebsgaming','koyo','zethiann','forcegaming','strippin'];
+var result = '';
+var logo ='';
+var channel_name ='';
+var channel_status ='';
+var channel_banner ='';
 function sendRequest(i) {
   console.log(channelsArray[i]);
   $.ajax({
     url:'https://api.twitch.tv/kraken/streams/'+ channelsArray[i] +'/?client_id=25uubqo7b284h9adfikgpgxn4xzub6',
     success: function(response) {
       if (response.stream) {
-        var logo = response.stream.channel.logo;
-        var channel_name = response.stream.channel.display_name;
-        var channel_status = response.stream.channel.game;
-        var channel_banner = response.stream.channel.profile_banner;
-        var result = '';
+        console.log(response);
+        logo = response.stream.channel.logo;
+        channel_name = response.stream.channel.display_name;
+        channel_status = response.stream.channel.game;
+        channel_banner = response.stream.channel.profile_banner;
         result += '<div class="channel_frame col-lg-6 col-md-6 col-sm-12 col-xs-12"><div class="padding_div"><div class="channel_banner" id="channel_banner"></div><p id="channel_on_off">Offline</p><img id="channel_logo" class="logo_img" src="" alt="channel logo"><h2 id="channel_name"></h2><p id="channel_status"></p></div></div>';
-        //$('.row').html(result);
+        $('#rowId').html(result);
         $('#channel_logo').attr('src',logo);
         $('#channel_name').text(channel_name);
         $('#channel_status').text(channel_status);
@@ -27,9 +31,8 @@ function sendRequest(i) {
             logo = response.logo;
             channel_name = response.display_name;
             channel_banner = response.profile_banner;
-            result = '';
             result += '<div class="channel_frame col-lg-6 col-md-6 col-sm-12 col-xs-12"><div class="padding_div"><div class="channel_banner" id="channel_banner"></div><p id="channel_on_off">Offline</p><img id="channel_logo" class="logo_img" src="" alt="channel logo"><h2 id="channel_name"></h2><p id="channel_status"></p></div></div>';
-            //$('.row').html(result);
+            $('#rowId').html(result);
             $('#channel_logo').attr('src',logo);
             $('#channel_name').text(channel_name);
             if (channel_banner) {
@@ -46,9 +49,8 @@ function sendRequest(i) {
 
 } // functions sendRequest() end
 
-
-for (var i = 0; i < channelsArray.length; i++) {
-  sendRequest(i);
-}
+  for (var j = 0; j < channelsArray.length; j++) {
+    sendRequest(j);
+  }
 
 });
